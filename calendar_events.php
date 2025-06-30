c0cc594e46540bec91323e5144ad8bab5ef5ee51
misaniapt <info.misaninovara@gmail.com>
2025-06-30 16:35:20 +0200

    Update calendar_events.php

--- calendar_events.php
+++ calendar_events.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+header('Content-Type: application/json');
 
 require_once __DIR__ . '/vendor/autoload.php';
 putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/google-calendar.json');
