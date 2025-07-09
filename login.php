ff0a874f030612c9b072cfab242475a2032bed03
misaniapt <info.misaninovara@gmail.com>
2025-07-09 12:22:52 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-echo "DEBUG INIZIO<br>"; // aggiungi questa riga!
+
 require_once __DIR__ . '/includes/db.php';
 require_once __DIR__ . '/includes/config.php';
 
