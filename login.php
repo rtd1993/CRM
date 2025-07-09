43c47f0d66403d7700902f9b807c1919e0ca7ac3
misaniapt <info.misaninovara@gmail.com>
2025-07-09 11:35:24 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,9 @@
 
 require_once __DIR__ . '/includes/db.php';
 require_once __DIR__ . '/includes/config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 $error = '';
