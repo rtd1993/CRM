077562123f84aad78b40a9e211917d7f39b18722
misaniapt <info.misaninovara@gmail.com>
2025-07-31 07:03:27 +0200

    v

--- email_cronologia.php
+++ email_cronologia.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user_id'])) {
 
 // Connessione database
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=crm;charset=utf8mb4', 'root', 'root', [
+    $pdo = new PDO('mysql:host=localhost;dbname=crm;charset=utf8mb4', 'crmuser', 'Admin123!', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
