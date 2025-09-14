1fabc8527829f31a19889c873365f4fb5dfc2526
misaniapt <info.misaninovara@gmail.com>
2025-09-14 11:44:10 +0200

    Update procedure.php

--- procedure.php
+++ procedure.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/includes/auth.php';
 require_login();
+require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/header.php';
 
 // Gestione eliminazione
