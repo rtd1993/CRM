12299d861bba3b7f4e15a7cde4f16a0e989ef8b1
misaniapt <info.misaninovara@gmail.com>
2025-09-14 17:59:31 +0200

    Update chat-complete.js

--- assets/js/chat-complete.js
+++ assets/js/chat-complete.js
@@ -982,7 +982,7 @@ class CompleteChatSystem {
             this.elements.privatesList.innerHTML = '';
             
             // Carica utenti dal database
-            const response = await fetch('api/utenti_for_chat.php');
+            const response = await fetch(`api/utenti_for_chat.php?user_id=${this.config.userId}`);
             
             if (!response.ok) {
                 throw new Error(`HTTP ${response.status}`);
