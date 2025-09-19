685b3928b0618284b47eb4c5d9578850e7905488
misaniapt <info.misaninovara@gmail.com>
2025-09-19 18:22:25 +0200

    Update richieste.php

--- richieste.php
+++ richieste.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/includes/auth.php';
 require_login();
 require_once __DIR__ . '/includes/config.php';
+
 require_once __DIR__ . '/includes/header.php';
 
 // Gestione eliminazione richiesta
